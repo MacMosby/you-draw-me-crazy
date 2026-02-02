264e9b4ffd40003ae4b02263bfbfb2e77b362b89
steffi <steffi.bap@gmail.com>
2026-02-02 16:19:32 +0100

    add: websocket module

--- requirements/backend/src/app.module.ts
+++ requirements/backend/src/app.module.ts
@@ -6,18 +6,24 @@
 /*   By: lieselotdetaeye <lieselotdetaeye@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/14 16:17:58 by lde-taey          #+#    #+#             */
-/*   Updated: 2026/02/09 11:30:36 by lieselotdet      ###   ########.fr       */
+/*   Updated: 2026/02/09 11:40:14 by lieselotdet      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-import { Module } from '@nestjs/common'
-// import { UsersModule } from './users/users.module'
-import { AuthModule} from  './auth/auth.module'
-import { AuthController } from './auth/auth.controller'
+import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
+import { UsersController } from './users/users.controller';
+import { DatabaseModule } from './database/database.module';
+import { WebsocketModule } from './websocket/websocket.module';
 
 @Module
 ({
-  imports: [AuthModule],
+  imports: [AuthModule,
+	UsersModule,
+	DatabaseModule,
+	WebsocketModule],
     //ConfigModule,//would need to be installe seperately
     //DatabaseModule,
     //AuthModule,
