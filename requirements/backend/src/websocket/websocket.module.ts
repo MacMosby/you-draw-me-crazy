d91c1b6b83611ed00a0bb676f3649c37db0cceba
steffi <steffi.bap@gmail.com>
2026-02-03 18:45:05 +0100

    add: connectionRegistry

--- requirements/backend/src/websocket/websocket.module.ts
+++ requirements/backend/src/websocket/websocket.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { WebsocketGateway } from './websocket.gateway';
+import { ConnectionRegistry } from './websocket.service';
 
 @Module({
-	providers: [WebsocketGateway],
+	providers: [WebsocketGateway, ConnectionRegistry],
+	exports: [ConnectionRegistry],
 })
 export class WebsocketModule {}
