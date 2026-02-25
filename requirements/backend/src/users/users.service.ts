568cfaf9843801ca08cdfe98100a04690262da74
Marc Rodenbusch <87020460+MacMosby@users.noreply.github.com>
2026-02-25 16:42:35 +0100

    add: implement removeFriend event

--- requirements/backend/src/users/users.service.ts
+++ requirements/backend/src/users/users.service.ts
@@ -54,4 +54,23 @@ export class UsersService {
 			});
 		}
 	}
+	async removeFriend(userID: number, friendID: number) {
+		const user = await this.prisma.user.findUnique({
+			where: { id: userID },
+			select: { friends: true },
+		});
+
+		if (!user) {
+			throw new Error("User not found");
+		}
+
+		const friends = user.friends ?? [];
+
+		if (friends.includes(friendID)) {
+			await this.prisma.user.update({
+				where: { id: userID },
+				data: { friends: friends.filter((id: number) => id !== friendID) },
+			});
+		}
+	}
 }
