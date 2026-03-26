7ad5c6a2b5a79bb1d293fde68eba6ffd4d9ca5e5
steffi <steffi.bap@gmail.com>
2026-03-26 10:06:39 +0100

    fix: backend internal https

--- requirements/backend/src/main.ts
+++ requirements/backend/src/main.ts
@@ -13,14 +13,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common'; //NB : To allow dto validation
-
+import * as fs from 'fs';
+//import * as https from 'https';
 
 async function bootstrap() 
 {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+	key: fs.readFileSync('/app/certs/backend.key'),
+	cert: fs.readFileSync('/app/certs/backend.crt'),
+  };
+  const app = await NestFactory.create(AppModule, {httpsOptions,});
   app.useGlobalPipes(new ValidationPipe()); // NB: dto validation only works if enabled.
   await app.listen(3000); // 3000 is the port
-  console.log('Server is running on http://localhost:3000');
+  console.log('Server is running on https://localhost:3000');
   console.log('Testing changes');
 }
 bootstrap();
