98a26769136d811f9b45ed0b0fc22438e0931bad
lotte-detaeye <lieselot.detaeye@gmail.com>
2026-04-08 18:20:09 +0200

    fix: logo responsive design

--- requirements/frontend/src/features/auth/logo.tsx
+++ requirements/frontend/src/features/auth/logo.tsx
@@ -5,7 +5,7 @@ export default function Logo() {
    <img
       src={logo}
       alt="You Draw Me Crazy logo"
-      className="h-88 w-auto rounded-2xl"
+      className="max-w-64 sm:max-w-96 max-h-64 sm:max-h-96 object-contain rounded-2xl"
     />
   )
 }
